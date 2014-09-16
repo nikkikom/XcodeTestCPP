61d6ff471f6b36894ba80be69932f60b10558e34
Nikki Chumakov <nikkikom@gmail.com>
2014-09-16 15:50:55 +0400

    Init commit

---
+++ hello.cc
@@ -0,0 +1,15 @@
+//
+//  hello.cc
+//  XcodeTestCPP
+//
+//  Created by Nikita Chumakov on 16/09/14.
+//  Copyright (c) 2014 Nikki Chumakov. All rights reserved.
+//
+
+#include <iostream>
+#include "hello.h"
+
+void foo ()
+{
+  std::cout << "hello world\n";
+}
---
+++ hello.h
@@ -0,0 +1,14 @@
+//
+//  hello.h
+//  XcodeTestCPP
+//
+//  Created by Nikita Chumakov on 16/09/14.
+//  Copyright (c) 2014 Nikki Chumakov. All rights reserved.
+//
+
+#ifndef XcodeTestCPP_hello_h
+#define XcodeTestCPP_hello_h
+
+
+
+#endif
